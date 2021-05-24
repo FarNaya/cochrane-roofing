e37071d01cdafdbaeef2e9b6f8dcec94b13b98fb
FarNaya <56936820+FarNaya@users.noreply.github.com>
2021-05-24 14:44:19 -0600

    update.

--- enviar2.php
+++ enviar2.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 $name = htmlspecialchars($_POST["name"]);
 $email = htmlspecialchars($_POST['email']);
 $phone = htmlspecialchars($_POST['phone']);
-$text = htmlspecialchars($_POST['text']);
+$comment = htmlspecialchars($_POST['comment']);
 
 $header = 'From: noreply@dominio.ca' . " \r\n";
 $header .= 'Reply-To:' . $email . " \r\n";
@@ -11,17 +11,17 @@ $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$text = "Message sent by" . $name . ",\r\n";
-$text .= "Email" . $email . " \r\n";
-$text .= "Message: " . $text . " \r\n";
-$text .= "Enviado el " . date('d/m/Y', time());
+$text = "Message sent by " . $name . ",\r\n";
+$text .= "Email: " . $email . " \r\n";
+$text .= "Phone number: " . $phone . " \r\n";
+$text .= "Message: " . $comment . " \r\n";
+$text .= "Sent " . date('d/m/Y', time());
 
-// $to = 'info@cochraneroofingalberta.ca';
 $to = 'faridanaya08@gmail.com';
-$subject = 'Mensaje de mi sitio web';
+$subject = 'Message from cochraneroofingalberta.ca';
 
 mail($to, $subject, utf8_decode($text), $header);
 
-header("Location:contact.html");
+header("Location:index.html");
 
 ?>
