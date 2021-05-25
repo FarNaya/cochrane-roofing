cf53db64b474d63503ae6685a2f1b449a0d91b8f
FarNaya <56936820+FarNaya@users.noreply.github.com>
2021-05-25 10:19:36 -0600

    update

--- emailsent.php
+++ emailsent.php
@@ -1,4 +1,23 @@
 <?php
+require "PHPMailer/Exception.php";
+require "PHPMailer/PHPMailer.php";
+require "PHPMailer/SMTP.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$oMail= new PHPMailer();
+$oMail->isSMTP();
+$oMail->Host="smtp.office365.com";
+$oMail->Port=587;
+$oMail->SMTPSecure="tls";
+$oMail->SMTPAuth=true;
+$oMail->Username="info@cochraneroofingalberta.ca";
+$oMail->Password="hierbabuena2021";
+$oMail->setFrom("info@cochraneroofingalberta");
+
+// 
+
 error_reporting(E_ALL);
 $name = htmlspecialchars($_POST["name"]);
 $email = htmlspecialchars($_POST['email']);
